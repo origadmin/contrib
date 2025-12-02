e434aa2b64b28f4ce186b599ba733ffc845a136f
godcong <jumbycc@163.com>
2025-12-02 19:48:44 +0800

    refactor(consul): restructure consul config package with adapter pattern and code generation

---
+++ config/consul/consul.go
@@ -0,0 +1,5 @@
+// Package consul implements the functions, types, and interfaces for the module.
+package consul
+
+//go:generate adptool .
+//go:adapter:package github.com/go-kratos/kratos/contrib/config/consul/v2
